6ef8d7daa36646e2d46f288f6b228f11b6a1e21f
Dan Gohman <dev@sunfishcode.online>
2021-05-31 13:35:18 -0700

    Add a note about the example not working on Windows.

--- examples/portable-views.rs
+++ examples/portable-views.rs
@@ -9,6 +9,9 @@ fn main() -> io::Result<()> {
     // `AsBorrowedFd`/`AsBorrowedHandle` can be viewed as any type
     // supporting `FromOwnedFilelike`, so you can call `File` methods
     // on `Stdout` or other things.
+    //
+    // Whether or not you can actually do this is up to the OS, of course.
+    // In this case, Unix can do this, but it appears Windows can't.
     let metadata = stdout.as_filelike_view::<File>().metadata()?;
 
     if metadata.is_file() {
