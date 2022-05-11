750b57d5438a07af1aaba25177ee4de3b05dc5f3
Dan Gohman <dev@sunfishcode.online>
2022-05-11 09:07:53 -0700

    RawSocket is unsigned on Windows.

--- tests/niche-optimizations.rs
+++ tests/niche-optimizations.rs
@@ -48,25 +48,24 @@ fn test_niche_optimizations_socket() {
         size_of::<RawSocket>(),
     );
     unsafe {
+        #[cfg(target_pointer_size = "32")]
+        let (min, max) = (i32::MIN as u32, i32::MAX as u32);
+        #[cfg(target_pointer_size = "64")]
+        let (min, max) = (i64::MIN as u64, i64::MAX as u64);
+
+        assert_eq!(OwnedSocket::from_raw_socket(min).into_raw_socket(), min);
+        assert_eq!(OwnedSocket::from_raw_socket(max).into_raw_socket(), max);
         assert_eq!(
-            OwnedSocket::from_raw_socket(RawSocket::MIN).into_raw_socket(),
-            RawSocket::MIN
-        );
-        assert_eq!(
-            OwnedSocket::from_raw_socket(RawSocket::MAX).into_raw_socket(),
-            RawSocket::MAX
-        );
-        assert_eq!(
-            Some(OwnedSocket::from_raw_socket(RawSocket::MIN))
+            Some(OwnedSocket::from_raw_socket(min))
                 .unwrap()
                 .into_raw_socket(),
-            RawSocket::MIN
+            min
         );
         assert_eq!(
-            Some(OwnedSocket::from_raw_socket(RawSocket::MAX))
+            Some(OwnedSocket::from_raw_socket(max))
                 .unwrap()
                 .into_raw_socket(),
-            RawSocket::MAX
+            max
         );
     }
 }
